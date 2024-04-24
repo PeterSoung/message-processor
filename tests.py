e893088d3b15fa3077e430017118d841ef68e5f8
psoung <psoung@kipsu.com>
2024-04-24 16:08:51 -0500

    Fixed unit test / added test_data_loads

--- tests.py
+++ tests.py
@@ -6,7 +6,13 @@ import json_reader
 class Test(unittest.TestCase):
 
     def test_incorrect_file_name(self):
-        self.assertEqual(json_reader.open_json_file("INCORRECT_FILE"), "Your file could not be found")
+        self.assertEqual(json_reader.Json_reader("INCORRECT_FILE").open_json_file(), "Your file could not be found")
 
     def test_non_json_file(self):
-        self.assertEqual(json_reader.open_json_file("json_files/non_json_file"), "Your file could not be found")
+        self.assertEqual(json_reader.Json_reader("json_files/non_json_file").open_json_file(), "Your file could not be found")
+
+    def test_data_loads(self):
+        data = json_reader.Json_reader("json_files/Guests.json")
+        data.open_json_file()
+        self.assertNotEqual(data.create_list_of_guest(), "")
+
